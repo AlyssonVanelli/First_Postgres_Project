e36d12930a769ddf05a4f315d409a96301c09e36
Alysson <aharon_curitiba@hotmail.com>
2022-07-11 14:12:23 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+(async () => {
+    const Planet = require('./models/Planet');
+
+    /*
+        const newPlanet = await Planet.create({
+            name: "Venus",
+            position: 5,
+        });
+    */
+
+    const seePlanets = await Planet.findByPk(1);
+
+    console.log(seePlanets)
+
+})();
